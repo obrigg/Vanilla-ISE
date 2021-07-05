bd36278259105ba974f9bb8e5ab0fa691d49d417
Oren Brigg <obrigg@cisco.com>
2021-07-05 14:24:43 +0300

    2021

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-""" Copyright (c) 2020 Cisco and/or its affiliates.
+""" Copyright (c) 2021 Cisco and/or its affiliates.
 This software is licensed to you under the terms of the Cisco Sample
 Code License, Version 1.1 (the "License"). You may obtain a copy of the
 License at
